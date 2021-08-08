8501e6aad74046f45b2b26c206af1f3f031403fe
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-08-08 12:46:01 +0530

    adds local storage

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -1,12 +1,28 @@
-import { createContext, useCallback, useReducer } from "react";
+import { createContext, useCallback, useEffect, useReducer } from "react";
 import { v4 as id } from "uuid";
 import { reducer, TODO_ADD, TODO_DELETE, TODO_DONE } from "./reducers";
 import initialData from "./initialData";
 
 export const TodoContext = createContext();
 
+const getStateFromLocalStorage = () => {
+  const storage = localStorage.getItem("todos");
+  console.log(storage);
+  if (storage) return JSON.parse(storage).todos;
+  return initialData;
+};
+
+const storeStateInLocalStorage = (todos) => {
+  localStorage.setItem("todos", JSON.stringify({ todos }));
+  console.log(localStorage);
+};
+
 export const TodoProvider = ({ children }) => {
-  const [todos, dispatch] = useReducer(reducer, initialData);
+  const initialState = getStateFromLocalStorage();
+  const [todos, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    storeStateInLocalStorage(todos);
+  }, [todos]);
   const addTodo = useCallback(
     ({ title, description }) => {
       dispatch({
